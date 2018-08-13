4842cd58b991ee10e2ab14f78ce741185af9e9fe
fintechilla <henryk.walerys@gmail.com>
2018-08-14 00:28:02 +0200

    Module 6.2 test of add and subtract

--- kodilla-testing/src/main/java/com/kodilla/testing/testingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/testingMain.java
@@ -1,9 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class testingMain {
 
     public static void main(String[] args){
-        System.out.println("Module 6 Introduction to code testing");
+        //System.out.println("Module 6 Introduction to code testing");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        if(calculator.add(1, 2) == (1 + 2)){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
 
     }
 }
