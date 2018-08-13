17446f7203f9e0ebf9148fda376e4f7cb6e0afe8
fintechilla <henryk.walerys@gmail.com>
2018-08-13 23:46:38 +0200

    Added build gradle and testingMain Module 6.1

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/testingMain.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing;
+
+public class testingMain {
+
+    public static void main(String[] args){
+        System.out.println("Module 6 Introduction to code testing");
+
+    }
+}
