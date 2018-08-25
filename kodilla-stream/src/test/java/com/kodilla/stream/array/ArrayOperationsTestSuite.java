248f074aa2da8904340705ca2b1efb98a863cacf
fintechilla <henryk.walerys@gmail.com>
2018-08-25 21:10:33 +0200

    Module 7.5 - Stream Average added and committed

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,33 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage() {
+
+        //Given
+        int n = 20;
+        int[] numbers = new int[n];
+        numbers = generateArray(n);
+
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(9.5, result, .1);
+
+
+    }
+    public int[] generateArray(int n){
+        int[] numbers = new int[n];
+        for(int i = 0; i < n; i++){
+            numbers[i] = i;
+        }
+        return numbers;
+    }
+
+
 }
