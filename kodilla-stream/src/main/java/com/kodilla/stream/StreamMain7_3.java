0ae067b15d9ec0be9bc53bfaf8e3ac71b5b3cd8f
fintechilla <henryk.walerys@gmail.com>
2018-08-25 13:26:56 +0200

    Module 7.3 - Stream - ForumUser, Forum

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain7_3.java
@@ -0,0 +1,37 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.forumUser.Forum;
+import com.kodilla.stream.forumUser.ForumUser;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMain7_3 {
+    public static void main(String[] args){
+        //ForumUser forumUser1 = new ForumUser(1, "John", 'M', LocalDate.of(2001, 01, 01));
+        List<ForumUser>forumUserList = new ArrayList<>();
+
+        /* forumUserList.add(forumUser1); */
+        Forum forum = new Forum(20);
+        forumUserList = forum.getForumUserList();
+        Map<Integer, ForumUser> theResultMapOfForumUserList = forumUserList.stream()
+                .filter(s->s.getGender()=='M')
+                .filter(s-> Period.between(s.getBirthDay(), LocalDate.now()).getYears() > 20)
+                .filter(s->s.getPostsQuantity()>1)
+                .collect(Collectors.toMap(ForumUser::getId, forumUser -> forumUser));
+
+        System.out.println("# elements: " + forumUserList.size());
+        theResultMapOfForumUserList.entrySet().stream()
+                .map(entry -> "entry: " + entry.getKey()
+                        + " name: " + entry.getValue().getName()
+                        + ", gender: " + entry.getValue().getGender()
+                        + ", Date of birth: " + entry.getValue().getBirthDay()
+                        + ", postsQuantity: " + entry.getValue().getPostsQuantity())
+                .forEach(System.out::println);
+    }
+
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumUser/ForumUser.java
@@ -0,0 +1,40 @@
+package com.kodilla.stream.forumUser;
+
+import java.time.LocalDate;
+
+public class ForumUser {
+    private final int id;
+    private final String name;
+    private final char gender;
+    private final LocalDate birthDay;
+    private int postsQuantity = 0;
+
+    public ForumUser(final int id, final String name, final char gender, final LocalDate birthDay, int postsQuantity) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.birthDay = birthDay;
+        this.postsQuantity = postsQuantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public int getPostsQuantity() {
+        return postsQuantity;
+    }
+
+}
