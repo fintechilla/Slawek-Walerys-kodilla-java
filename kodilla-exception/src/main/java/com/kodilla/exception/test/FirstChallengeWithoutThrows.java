f9df688b80369d5ce38ac24fffccba948191db0c
fintechilla <henryk.walerys@gmail.com>
2018-09-02 00:47:47 +0200

    Module 8.2 - another attempt - try-catch is not thrown outside, it is inside the double method. I am not sure whether it works fine

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallengeWithoutThrows.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallengeWithoutThrows.java
@@ -1,4 +1,22 @@
 package com.kodilla.exception.test;
 
 public class FirstChallengeWithoutThrows {
+    public double divide(double a, double b) {
+        double result = 0;
+        try {
+            result =  a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Exception: Division by ZERO is not allowed, message: " + e);
+        } finally {
+            System.out.println("We did it!!");
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        FirstChallengeWithoutThrows firstChallenge = new FirstChallengeWithoutThrows();
+        double result = firstChallenge.divide(3, 0);
+            System.out.println("Result is " + result);
+    }
 }
