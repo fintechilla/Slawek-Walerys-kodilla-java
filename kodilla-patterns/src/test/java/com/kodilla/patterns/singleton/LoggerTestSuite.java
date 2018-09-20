16f27f657d26b0267c44ef9ea561aea716acba06
fintechilla <henryk.walerys@gmail.com>
2018-09-20 23:30:25 +0200

    Module 11.1 - Logger - Singleton

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    private static Logger logger = new Logger();
+    @Test
+    public void testLastLog(){
+        //Given
+        logger.getInstance().log("first");
+        logger.getInstance().log("second");
+        //When
+        String logName = logger.getInstance().getLastLog();
+        System.out.println("Last log: " + logName);
+        //Then
+        Assert.assertEquals("second", logName);
+    }
+}
