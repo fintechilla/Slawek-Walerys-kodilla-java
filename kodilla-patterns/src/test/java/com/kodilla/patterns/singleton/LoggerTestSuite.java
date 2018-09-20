bbda1a43726b812c5abb89975fdd2e786eb7ce87
fintechilla <henryk.walerys@gmail.com>
2018-09-20 23:40:43 +0200

    Module 11.1 - Logger - Singleton - correction for private constructor

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LoggerTestSuite {
-    private static Logger logger = new Logger();
+    private static Logger logger ;
     @Test
     public void testLastLog(){
         //Given
