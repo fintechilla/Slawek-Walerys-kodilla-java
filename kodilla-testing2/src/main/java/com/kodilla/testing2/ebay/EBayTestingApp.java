622d835780092dbef20a9ab0397339ad17c82faa
fintechilla <henryk.walerys@gmail.com>
2019-03-09 19:32:17 +0100

    Mod 23.2 - Ebay search laptop

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,20 +1,18 @@
-package com.kodilla.testing2.google;
+package com.kodilla.testing2.ebay;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class GoogleTestingApp {
-    public static final String SEARCHFIELD = "gLFyf";//"gsfi";
-
+public class EBayTestingApp {
+    public static final String SEARCHFIELD = "gh-ac";//"gsfi";
     public static void main(String[] args){
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-        driver.get("https://www.google.com");
+        driver.get("https://www.ebay.com");
 
-        WebElement searchField = driver.findElement(By.className(SEARCHFIELD));
-        searchField.sendKeys("Kodilla");
+        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
+        searchField.sendKeys("laptop");
         searchField.submit();
-
     }
 }
